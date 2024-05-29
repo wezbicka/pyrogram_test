e382dbe5c5834c68e103b80b43003bfffad90ed5
Polina Vezhbitskaya <75391420+wezbicka@users.noreply.github.com>
2024-05-29 15:30:00 +0300

    Delete example environment variable

--- app/config.py
+++ app/config.py
@@ -4,17 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TELEGRAM_BOT_TOKEN = getenv('TELEGRAM_BOT_TOKEN', '123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11')
+TELEGRAM_BOT_TOKEN = getenv('TELEGRAM_BOT_TOKEN')
 
-DATABASE_CONNECTION_STRING = getenv(
-    'DATABASE_CONNECTION_STRING',
-    'postgresql://postgres:postgres@db:5432/pyrogram_db'
-)
+DATABASE_CONNECTION_STRING = getenv('DATABASE_CONNECTION_STRING')
 
-SECRET_KEY = getenv("SECRET_KEY", "Nici5kgdDugUeddUQk4Vehclq1QYoZW4oO7uVguCnlU=")
+SECRET_KEY = getenv("SECRET_KEY")
 
-API_HASH = getenv('API_HASH', '0123456789abcdef0123456789abcdef')
+API_HASH = getenv('API_HASH')
 
-API_ID = int(getenv('API_ID', '12345678'))
+API_ID = int(getenv('API_ID'))
 
 CLIENT_SESSION_PATH = getenv('CLIENT_SESSION_PATH', './app/bot_init')
